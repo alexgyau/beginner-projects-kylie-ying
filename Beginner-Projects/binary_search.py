9e52d8448ab027fb0e212eda90c4de58f8059f9a
alex gyau <akwaaidc@gmail.com>
2026-01-27 07:58:41 -0600

    created a new file for binary search

---
+++ Beginner-Projects/binary_search.py
@@ -0,0 +1,8 @@
+# Implementation of binary search algorithm!
+
+# we will prove that binary search is faster than naive search!
+
+# naive search: scan entire list and ask if its equal to the target
+# if yes, return the index
+# if no, then return -1
+
